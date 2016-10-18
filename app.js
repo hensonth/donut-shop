d767b366d2973752abf6d6db2a3a0dd68f04efe1
Henson Tay <hensontay@gmail.com>
2016-10-18 17:55:40 +0800

    fixed routing issue to land on main page

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ console.log('the environment is on' + process.env.NODE_ENV);
 
 if(process.env.NODE_ENV === 'production') {
 // heroku mongo connection
-  mongoose.connect('mongodb://hensonth:abcabc123@ds061076.mlab.com:61076/wdi6-henson/donuts')
+  mongoose.connect('mongodb://hensonth:abcabc123@ds061076.mlab.com:61076/wdi6-henson/')
   console.log('connected')
 } else {
   //local host mongo connection
@@ -25,7 +25,7 @@ app.use(express.static(__dirname + '/public'))
 var frontendRoutes = require('./routes/donuts')
 var ajaxRoutes = require('./routes/donuts_api')
 
-app.use('/donuts', frontendRoutes) // only render ejs files
+app.use('/', frontendRoutes) // only render ejs files
 app.use('/api/donuts', ajaxRoutes) // only handle ajax request
 
 app.listen(process.env.PORT || 3000)
